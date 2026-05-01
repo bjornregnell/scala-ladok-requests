e642be6b35f513435ca5ebe146aa9b01a20c34e3
bjornregnell <bjorn.regnell@cs.lth.se>
2026-05-01 16:31:05 +0200

    bump version

--- publish.scala
+++ publish.scala
@@ -1,8 +1,8 @@
+//> using publish.name scala-ladok-requests
+//> using publish.version 0.2.0
 //> using publish.organization se.bjornregnell
 //> using publish.license "Apache-2.0"
 //> using publish.developer "bjornregnell|Björn Regnell|https://bjornregnell.se"
 //> using publish.scm github:bjornregnell/scala-ladok-requests
 //> using publish.url https://github.com/bjornregnell/scala-ladok-requests
 //> using publish.repository central
-//> using publish.name scala-ladok-requests
-//> using publish.version 0.1.0
