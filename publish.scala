739fa1c46787ff59d1076e0de00e02870403ac7e
bjornregnell <bjorn.regnell@cs.lth.se>
2026-05-08 12:37:24 +0200

    add info on silence warning on multi-file using-directives

--- publish.scala
+++ publish.scala
@@ -6,3 +6,7 @@
 //> using publish.scm github:bjornregnell/scala-ladok-requests
 //> using publish.url https://github.com/bjornregnell/scala-ladok-requests
 //> using publish.repository central
+
+
+// you can silence the warning about multi-file using-directives like so:
+// scala-cli config suppress-warning.directives-in-multiple-files true
